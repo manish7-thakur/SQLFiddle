5f31e8fbc359f181e0594ff9d6a2017bc1083c9c
mthakur <manish.thakur@agoda.com>
2022-04-08 12:45:00 +0700

    rectify column name

--- src/watch-time.sql
+++ src/watch-time.sql
@@ -1,14 +1,15 @@
-create table watch_times(id integer, category varchar(50), w_time integer);
-insert into watch_times values(1, "drama", 5);
-insert into watch_times values(2, "sports", 5);
-insert into watch_times values(3, "drama", 4);
-insert into watch_times values(4, "sports", 11);
-insert into watch_times values(5, "sports", 11);
+create table Videos(id integer, category varchar(50), watch_time integer);
+insert into Videos values(1, "drama", 5);
+insert into Videos values(2, "sports", 5);
+insert into Videos values(3, "drama", 4);
+insert into Videos values(4, "sports", 11);
+insert into Videos values(5, "sports", 11);
 
 -- select id, category, w_time with highest w_time in each category
 -- id	category	w_time
 -- 1	drama	       5
 -- 4	sports	       11
 -- 5	sports	       11
-select w.id, w.category, w.w_time from watch_times w inner join
-                                       (select category, max(w_time) as w_time from watch_times group by category) t on w.w_time = t.w_time and w.category = t.category;
+
+select v.id, v.category, v.watch_time from Videos v inner join
+                                           (select category, max(watch_time) as watch_time from Videos group by category) t on v.watch_time = t.watch_time and v.category = t.category;
